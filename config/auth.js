5bcaa5d86fa8d5dca5cf463332bd36c9a2c572a1
alu0100505009 <alu0100505009@ull.edu.es>
2016-05-20 15:16:40 +0000

    Creado auth.js

---
+++ config/auth.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+    'googleAuth' : {
+        'clientID'      : '38547233288-8ggk3d566p0vd260a7bjh084e6f70s2g.apps.googleusercontent.com',
+        'clientSecret'  : 'empSBaFhyA1NGkxrxe3QPeQv',
+        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+    }
+
+};
