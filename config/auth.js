acaaf541f04e51fb9db4d1690fbbe27eac661919
alu0100505009 <alu0100505009@ull.edu.es>
2016-05-23 10:50:19 +0000

    Login de google+ funcionando

--- config/auth.js
+++ config/auth.js
@@ -1,9 +1,9 @@
 module.exports = {
 
     'googleAuth' : {
-        'clientID'      : '656783998389-p5hac6phbcetfv1nj2rtvcgvn934t85u.apps.googleusercontent.com',
-        'clientSecret'  : '99hbhN7ncPDagko3PL-9M9t0',
-        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+        'clientID'      : '38547233288-d0kac8fqe1pnmks5pmgn4qigk1lske9m.apps.googleusercontent.com',
+        'clientSecret'  : 'drGqFgZcREo5PjZ-B8Lum-gL',
+        'callbackURL'   : 'https://prueba-alu0100505009.c9users.io:8080/auth/google/callback'
     }
 
 };
