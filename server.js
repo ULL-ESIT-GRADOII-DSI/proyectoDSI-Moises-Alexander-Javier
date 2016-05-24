ecbe915f6123c74320934699f6e390a320b62639
alu0100767421 <alu0100767421@ull.edu.es>
2016-05-24 00:36:06 +0000

    Arreglando problema para asignar los estilos

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 
 app.use(express.static(__dirname + '/app'));
-
+app.use(express.static(__dirname + '/public'));
 
 // routes ======================================================================
 require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
